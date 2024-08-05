251413d5a68038a3f2bb5548cbc8f538b0dc592c
Andrew Maksimchuk <32966111+AndrewMaksimchuk@users.noreply.github.com>
2024-08-05 09:31:30 +0300

    set same length

--- service.logger.ts
+++ service.logger.ts
@@ -1,10 +1,10 @@
 export class Logger {
     public static log(...messages: string[]) {
-        console.log('%c [ LOG ] ', 'background-color: white; color: black;', ...messages)
+        console.log('%c [ LOG     ] ', 'background-color: white; color: black;', ...messages)
     }
 
     public static error(...messages: string[]) {
-        console.error('%c [ ERROR ] ', 'background-color: red; color: black;', ...messages)
+        console.error('%c [ ERROR   ] ', 'background-color: red; color: black;', ...messages)
     }
 
     public static warning(...messages: string[]) {
