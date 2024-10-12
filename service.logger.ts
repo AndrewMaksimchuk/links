3972084508edcb336432f135a43b8074e8ca3370
Andrew Maksimchuk <32966111+AndrewMaksimchuk@users.noreply.github.com>
2024-10-12 17:25:30 +0300

    on dev save logs to file, simple logs #24

--- service.logger.ts
+++ service.logger.ts
@@ -1,16 +1,43 @@
+type Status = "LOG" | "ERROR" | "WARNING"
+
 export class Logger {
+    private static setDateTime() {
+        const length = 23
+        const moment = new Date(Date.now())
+        return `[ ${moment.toLocaleString().padEnd(length)} ]`;
+    }
+
+    private static setStatus(status: Status) {
+        const length = 8
+        return `[ ${status}` + ' '.repeat(length - status.length) + ']';
+    }
+
+    private static metadata(status: Status) {
+        return `${this.setStatus(status)} ${this.setDateTime()}`;
+    }
+
+    private static setMessage(status: Status, messages: string[]) {
+        return `${this.metadata(status)} ` + messages.join('; ');
+    }
+
     public static log(...messages: string[]) {
         if ('test' === process.env.NODE_ENV) return;
-        console.log('%c [ LOG     ] ', 'background-color: white; color: black;', ...messages)
+        const log = this.setMessage("LOG", messages)
+        console.log(log)
+        return log;
     }
 
     public static error(...messages: string[]) {
         if ('test' === process.env.NODE_ENV) return;
-        console.error('%c [ ERROR   ] ', 'background-color: red; color: black;', ...messages)
+        const log = this.setMessage("ERROR", messages)
+        console.log(log)
+        return log;
     }
 
     public static warning(...messages: string[]) {
         if ('test' === process.env.NODE_ENV) return;
-        console.warn('%c [ WARNING ] ', 'background-color: yellow; color: black;', ...messages)
+        const log = this.setMessage("WARNING", messages)
+        console.log(log)
+        return log;
     }
 }
